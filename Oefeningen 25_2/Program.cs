614ca3abc696531b01f2a65251e6382dce140cfd
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-25 10:19:34 +0100

    Add project files.

---
+++ Oefeningen 25_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefeningen_25_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mijnstring = @"C:\Users\Field\Documents\Oefening";
+            Console.WriteLine(mijnstring);
+            Console.Read();
+        }
+    }
+}
