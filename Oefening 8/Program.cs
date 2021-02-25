f9ae3971065026563190a3aa516bf177039a11c9
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-25 10:52:54 +0100

    tem oefening 9

--- Oefening 8/Program.cs
+++ Oefening 8/Program.cs
@@ -7,8 +7,12 @@ namespace Oefening_8
     {
         static void Main(string[] args)
         {
-               
 
+            string mijnString = "Dit is een mooie string uwu";
+            Console.WriteLine(mijnString.ToLower());
+            Console.WriteLine(mijnString.Substring(4,1));
+            
+            Console.ReadLine();
 
         }
     }
---
+++ Oefening9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string mijnString = "dit is een mooie string uwussssssss";
+            for (int i = 0; i < mijnString.Length; i++)
+            {
+                if (mijnString.Substring(i,1).Equals("e"))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
