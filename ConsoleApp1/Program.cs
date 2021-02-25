6f60c5d36e4baafb8818657129991966b86f79ec
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-25 10:26:16 +0100

    Oefningen 1 tot 3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal1, getal2, temp;
+            getal1 = 5;
+            getal2 = 85;
+            Console.WriteLine(getal1 + " " + getal2);
+            temp = getal1;
+            getal1 = getal2;
+            getal2 = temp;
+            
+            Console.WriteLine(getal1 + " " + getal2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Oefening2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mijnstring = "Ken zei: \"Dit is een toffe les!\"";
+            Console.WriteLine(mijnstring);
+            Console.ReadLine();
+        }
+    }
+}
